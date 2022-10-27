0368765fa011b3d89879e834e43466bf68f51e80
L140-beep <kartoshka0302@mail.ru>
2022-10-27 19:59:59 +0800

    added styles

--- main.kt
+++ main.kt
@@ -4,19 +4,28 @@ data class Message(val address: String?, val topic: String?, val name: String?, 
 
 fun main() {
     fun Message.toHTML(): String {
-        val template = "<table> " +
+        val style = "<head>\n" +
+                "\t<style>\n" +
+                "\t\tbody { \n" +
+                "\t\t\tbackground: rgb(14, 88, 12);\n" +
+                "\t\t\tfont-family: 'Times New Roman' ;\n" +
+                "\t\t}\n" +
+                "\t</style>\n" +
+                "</head>\n" +
+                "\n<body>\n"
+        var template = style + "<table> " +
                 (address?.let { "\n\t<tr>\n\t\t<td>address</td>\n\t\t<td>$it</td>\n\t</tr>" }
-                    ?: run{"\n\t<tr>\n\t\t<td> </td>\n\t</tr>"}) +
+                    ?: "\n\t<tr>\n\t\t<td> </td>\n\t</tr>") +
                 (topic?.let { "\n\t<tr>\n\t\t<td>topic</td>\n\t\t<td>$it</td>\n\t</tr>" }
-                    ?: run{"\n\t<tr>\n\t\t<td> </td>\n\t</tr>"}) +
+                    ?: "\n\t<tr>\n\t\t<td> </td>\n\t</tr>") +
                 (name?.let { "\n\t<tr>\n\t\t<td>Name</td>\n\t\t<td>$it</td>\n\t</tr>" }
-                    ?: run{"\n\t<tr>\n\t\t<td> </td>\n\t</tr>"}) +
+                    ?: "\n\t<tr>\n\t\t<td> </td>\n\t</tr>") +
                 (surname?.let { "\n\t<tr>\n\t\t<td>Surname</td>\n\t\t<td>$it</td>\n\t</tr>" }
-                    ?: run{"\n\t<tr>\n\t\t<td> </td>\n\t</tr>"}) +
-                "\n</table>"
+                    ?: "\n\t<tr>\n\t\t<td> </td>\n\t</tr>") +
+                "\n</table>" + "\n</body>"
         return template
     }
 
-    val m = Message("blabla@mail.ru", null, "Ivan", "Sergeevich")
+    val m = Message("blabla@mail.ru", null, null, null)
     println(m.toHTML())
 }
